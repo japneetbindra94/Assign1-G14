47f6e06b2dfbbcf0c16b52bde1f3d7f511885dc3
Japneet Kaur <japneetkaur@Japneets-MacBook-Air.local>
2026-01-13 23:30:27 -0800

    Finished Python algorithm for assignment

--- python_impl/sort_definitions.py
+++ python_impl/sort_definitions.py
@@ -1,13 +1,26 @@
-def load_defination(filename);
-with open(filename, "r", encoding="utf-8") as file:
-
-lines = file.readlines()
- definitions = [line.strip() for line in lines if line.strip() != ""]
+def load_definitions(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+    definitions = [line.strip() for line in lines if line.strip() != ""]
     return definitions
 
 definitions = load_definitions("../data/definitions.txt")
 
+memorized = ["Stack", "Queue"]
+
+non_mem = []
+mem = []
+
 for d in definitions:
+    if d in memorized:
+        mem.append(d)
+    else:
+        non_mem.append(d)
+
+non_mem.sort()
+
+final_list = non_mem + mem
+
+print("Final sorted list:")
+for d in final_list:
     print(d)
-
-
